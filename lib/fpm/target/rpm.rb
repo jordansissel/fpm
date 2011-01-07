fb53230879e7e278e854dc1a6f7122068eb2e1e9
Jordan Sissel <jls@semicomplete.com>
2011-01-07 00:50:06 -0800

    - Make rpm target work - Get fpm -s rpm -t rpm working (should provide a useful test case) - Allow 'output' (-p <path>) to be a directory

--- lib/fpm/target/rpm.rb
+++ lib/fpm/target/rpm.rb
@@ -1,28 +1,19 @@
 require "fpm/package"
 
 class FPM::Target::Rpm < FPM::Package
-  def assemble(params)
-    # TODO [Jay] a lot of this is duplication from deb.rb,
-    # and can be factored out.
+  def specfile(builddir)
+    "#{builddir}/#{name}.spec"
+  end
 
+  def build!(params)
     raise "No package name given. Can't assemble package" if !@name
-
-    root = params['root'] || '.'
-    paths = params['paths']
-    output = params['output']
-
-    type = "rpm"
-
-    output.gsub!(/VERSION/, "#{@version}-#{@iteration}")
-    output.gsub!(/ARCH/, @architecture)
-
-    builddir = "#{Dir.pwd}/build-#{type}-#{File.basename(output)}"
-
-    Dir.mkdir(builddir) if !File.directory?(builddir)
-
-    Dir.chdir root do
-      tar("#{builddir}/data.tar", paths)
-      system(*["gzip", "-f", "#{builddir}/data.tar"])
-    end
+    Dir.mkdir("BUILD")
+    args = ["rpmbuild", "-ba", 
+           "--define", "buildroot #{Dir.pwd}/BUILD",
+           "--define", "_topdir #{Dir.pwd}",
+           "--define", "_sourcedir #{Dir.pwd}",
+           "--define", "_rpmdir #{params[:output]}",
+           "#{name}.spec"]
+    system(*args)
   end
 end
