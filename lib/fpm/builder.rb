570098480cd0a6ddd6d64b8acbb13603587953fd
Jordan Sissel <jls@semicomplete.com>
2011-01-13 15:34:40 -0800

    - Fix issue/2 - we were accidentally ignoring deps specified via   commandline.

--- lib/fpm/builder.rb
+++ lib/fpm/builder.rb
@@ -37,8 +37,10 @@ class FPM::Builder
       :prefix => settings.prefix
     )
 
-    @package = package_class_for(settings.package_type).new(@source)
     @paths = paths
+    @package = package_class_for(settings.package_type).new(@source)
+    # Append dependencies given from settings (-d flag for fpm)
+    @package.dependencies += settings.dependencies
 
     @output = settings.package_path
     @recurse_dependencies = settings.recurse_dependencies
