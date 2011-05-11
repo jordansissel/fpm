42c0bfbcb265737676299ac7c0b13d8470b88440
Jordan Sissel <jls@semicomplete.com>
2011-05-11 15:49:08 -0700

    - add TODO

--- lib/fpm/package.rb
+++ lib/fpm/package.rb
@@ -71,6 +71,8 @@ class FPM::Package
     @license = source[:license] || "unknown"
     @maintainer = source[:maintainer] || "<#{ENV["USER"]}@#{Socket.gethostname}>"
     @architecture = source[:architecture] || %x{uname -m}.chomp
+    # RPM barfs on blank lines in the description. Hack around it.
+    # TODO(sissel): This belongs in the rpm template, not here.
     @description = source[:description].gsub(/^\s*$/," .") || "no description given"
     @provides = source[:provides] || []
     @scripts = source[:scripts]
