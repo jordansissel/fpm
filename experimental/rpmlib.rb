3602fdc8eadafa75e2997e2a471c8b1dbb328aae
Jordan Sissel <jls@semicomplete.com>
2011-05-13 01:03:56 -0700

    - start working on signature fetching

--- experimental/rpmlib.rb
+++ experimental/rpmlib.rb
@@ -24,7 +24,8 @@ class RPMFile
     #}
     
     def read(file)
-      data = file.read(96).unpack("A4CCssa66ssa16")
+      # Use 'A' here instead of 'a' to trim nulls.
+      data = file.read(96).unpack("A4CCnnA66nnA16")
       @magic, @major, @minor, @type, @archnum, @name, \
         @osnum, @signature_type, @reserved = data
       return nil
@@ -32,10 +33,52 @@ class RPMFile
 
     def write(file)
       data = [ @magic, @major, @minor, @type, @archnum, @name, \
-               @osnum, @signature_type, @reserved ].pack("A4CCssa66ssa16")
+               @osnum, @signature_type, @reserved ].pack("a4CCnnA66nna16")
       file.write(data)
     end # def write
   end # class ::RPMFile::Lead
+
+  class Signature
+    HEADER_SIGNED_TYPE = 5
+    HEADER_MAGIC = "\x8e\xad\xe8\x01\x00\x00\x00\x00"
+
+    attr_accessor :magic  # 8-byte string magic
+
+    # is 'il' index length?
+    attr_accessor :il     # rpmlib calls this field 'il' unhelpfully
+
+    # what is 'dl' ?
+    attr_accessor :dl     # rpmlib calls this field 'dl' unhelpfully
+
+    def read(file)
+      # Signature reads 16 bytes (4 x int32)
+      data = file.read(16).unpack("a8NN")
+      @magic, @il, @dl = data
+      validate
+      # data[0..1] is compared against rpm_header_magic
+      # data[2] must be between [0, 32] inclusive
+      # data[3] must be between [0, 8192] inclusive
+    end # def raed
+
+    def write(file)
+    end # def write
+
+    def validate
+      # TODO(sissel): raise real exceptions
+      if @magic != ::RPMFile::Signature::HEADER_MAGIC
+        raise "Signature magic did not match; got #{@magic.inspect}, " \
+              "expected #{::RPMFile::Signature::HEADER_MAGIC.inspect}"
+      end
+
+      if !(0..32).include?(@il)
+        raise "Invalid 'il' value #{@il}, expected to be in range [0..32]"
+      end
+
+      if !(0..8192).include?(@dl)
+        raise "Invalid 'il' value #{@dl}, expected to be in range [0..8192]"
+      end
+    end # def validate
+  end # class ::RPMFile::Signature
 
   def lead
     if @lead.nil?
@@ -46,7 +89,20 @@ class RPMFile
   end # def lead
 
   def signature
-    # Not supported yet.
+    lead
+
+    # If signature_type is not 5 (HEADER_SIGNED_TYPE), no signature.
+    if @lead.signature_type != Signature::HEADER_SIGNED_TYPE
+      @signature = false
+      return
+    end
+
+    if @signature.nil?
+      @signature = ::RPMFile::Signature.new
+      @signature.read(@file)
+    end
+
+    return @signature
   end # def signature
 
   def header
