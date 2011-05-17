952ff93aff46e3a6fef785c4f7a90f827fc6fc3a
Jordan Sissel <jls@semicomplete.com>
2011-05-17 00:17:22 -0700

    - Add general way for source plugins to add flags specific to that   plugin. For now, only 'gem' uses this. - Add flag --gem-bin-path to install a gem's executables to a specific   place. Some folks like /usr/bin, so, package puppet with bins there:   * fpm -s gem -t deb --gem-bin-path /usr/bin puppet
      The original implementation for this bin-path flag was by lassizci.
      https://github.com/jordansissel/fpm/pull/27

---
+++ lib/fpm/flags.rb
@@ -0,0 +1,20 @@
+require "fpm/namespace"
+
+class FPM::Flags
+  def initialize(opts, flag_prefix, help_prefix)
+    @opts = opts
+    @flag_prefix = flag_prefix
+    @help_prefix = help_prefix
+  end # def initialize
+
+  def on(*args, &block)
+    fixed_args = args.collect do |arg|
+      if arg =~ /^--/
+        "--#{@flag_prefix}-#{arg.gsub(/^--/, "")}" 
+      else
+        "(#{@help_prefix}) #{arg}"
+      end
+    end
+    @opts.on(*fixed_args, &block)
+  end # def on
+end # class FPM::Flags
