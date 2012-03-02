7d62f7c91b9673a7e318f0f5dfb7fb4e24533448
Jordan Sissel <jls@semicomplete.com>
2012-03-02 01:20:49 -0800

    - style and docs

--- lib/fpm/util.rb
+++ lib/fpm/util.rb
@@ -1,17 +1,35 @@
 require "fpm/namespace"
 
+# Some utility functions
 module FPM::Util
+  # Raised if safesystem cannot find the program to run.
+  class ExecutableNotFound < StandardError; end
+
+  # Raised if a safesystem program exits nonzero
+  class ProcessFailed < StandardError; end
+
+  # Run a command safely in a way that gets reports useful errors.
   def safesystem(*args)
     program = args[0]
-    # TODO(sissel): Search PATH for program, abort if we can't find it
+
+    # Scan path to find the executable
+    # Do this to help the user get a better error message.
+    if !program.include("/")
+      envpath = ENV["PATH"].split(":")
+      if envpath.select { |p| File.executable?(File.join(p, program)) }.empty?
+        raise ExecutableNotFound.new(program)
+      end
+    end
 
     success = system(*args)
     if !success
-      raise "#{args.first} failed with exit code #{$?.exitstatus}. Full command was: #{args.inspect}"
+      raise ProcessFailed.new("#{program} failed (exit code #{$?.exitstatus})" \
+                              ". Full command was:#{args.inspect}")
     end
     return success
   end # def safesystem
 
+  # Get the recommended 'tar' command for this platform.
   def tar_cmd
     # Rely on gnu tar for solaris and OSX.
     case %x{uname -s}.chomp
