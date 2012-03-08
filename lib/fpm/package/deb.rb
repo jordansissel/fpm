a1d4a423fd6ed58ef90175b2df402a4798b77329
Jordan Sissel <jls@semicomplete.com>
2012-03-08 07:49:02 -0800

    - more work aligning the deb package class with the new API

--- lib/fpm/package/deb.rb
+++ lib/fpm/package/deb.rb
@@ -9,14 +9,12 @@ class FPM::Package::Deb < FPM::Package
   # Installed size
   attr_accessor :installed_size
 
-  # Supported Debian control files
-  CONTROL_FILES = {
-    "pre-install"       => [:script, "preinst"],
-    "post-install"      => [:script, "postinst"],
-    "pre-uninstall"     => [:script, "prerm"],
-    "post-uninstall"    => [:script, "postrm"],
-    "debconf-config"    => [:script, "config"],
-    "debconf-templates" => [:text,   "templates"]
+  # Map of what scripts are named.
+  SCRIPT_MAP = {
+    :before_install     => "preinst",
+    :after_install      => "postinst",
+    :before_remove      => "prerm",
+    :after_remove       => "postrm",
   }
 
   option "--ignore-iteration-in-dependencies", :flag, 
@@ -42,7 +40,7 @@ class FPM::Package::Deb < FPM::Package
      File.expand_path(config)
   end
     
-    # Add custom debconf templates file
+  # Add custom debconf templates file
   option "--templates", "FILEPATH",
     "Add FILEPATH as debconf templates file." do |templates|
     File.expand_path(templates)
@@ -58,9 +56,8 @@ class FPM::Package::Deb < FPM::Package
   # native 'architecture' value should be.
   def architecture
     if @architecture.nil? or @architecture == "native"
-      # Default architecture should be 'native' which we'll need
-      # to ask the system about.
-      # TODO(sissel): only run dpkg if we can find it in the path.
+      # Default architecture should be 'native' which we'll need to ask the
+      # system about.
       if program_in_path?("dpkg")
         @architecture = %x{dpkg --print-architecture 2> /dev/null}.chomp
         @architecture = %{uname -m}.chomp if $?.exitstatus != 0
@@ -90,7 +87,7 @@ class FPM::Package::Deb < FPM::Package
     end
 
     if @name.include?("_")
-      @logger.info("Package name ncludes underscores, converting to dashes",
+      @logger.info("Package name includes underscores, converting to dashes",
                    :name => @name)
       @name = @name.gsub(/[_]/, "-")
     end
@@ -99,38 +96,17 @@ class FPM::Package::Deb < FPM::Package
   end # def name
 
   def output(output_path)
-    # Use custom Debian control file when given ...
-    FileUtils.mkdir(build_path("control"))
-
-    # TODO(sissel): Support the flag letting people specify their own control file
-    # TODO(sissel): --deb-custom-control
-    # TODO(sissel): Support putting the maintainer scripts in the controldir
-    # TODO(sissel): Make sure the maintainer scripts are executable
-    # TODO(sissel): Write 'config_files' to controldir/'conffiles'
-
-    with(control_path("control")) do |control|
-      control_data = template("deb.erb").result(binding)
-      @logger.debug("Writing control file", :path => control)
-      File.write(control, control_data)
-    end
-
-    # Make the control.tar.gz
-    with(build_path("control.tar.gz")) do |controltar|
-      @logger.info("Creating", :path => controltar, :from => control_path)
-      safesystem(tar_cmd, "--numeric-owner", "--owner=0", "--group=0", "-zcf",
-                 controltar, "-C", control_path, ".")
-    end
-    @logger.debug("Removing no longer needed control dir", :path => control_path)
-    FileUtils.rm_r(build_path("control"))
-
-    # create debian-binary file, required to make a valid debian package
+    # create 'debian-binary' file, required to make a valid debian package
     File.write(build_path("debian-binary"), "2.0")
 
-    # TODO(sissel): Tar up the staging_dir and call it 'data.tar.gz'
+    write_control_tarball
+
+    # Tar up the staging_dir and call it 'data.tar.gz'
     datatar = build_path("data.tar.gz")
     safesystem(tar_cmd, "-C", staging_path, "-zcf", datatar, ".")
 
-    # pack up the .deb
+    # pack up the .deb, which is just an 'ar' archive with 3 files
+    # the 'debian-binary' file has to be first
     with(File.expand_path(output_path)) do |output_path|
       ::Dir.chdir(build_path) do
         safesystem("ar", "-qc", output_path, "debian-binary", "control.tar.gz", datatar)
@@ -203,12 +179,6 @@ class FPM::Package::Deb < FPM::Package
     end
   end # def fix_dependency
 
-  # TODO(sissel): support pre_dependencies
-  #def pre_dependencies
-    #self.settings[:pre_depends] || []
-  #end # def pre_dependencies
-  #
-  
   def control_path(path=nil)
     @control_path ||= build_path("control")
     File.mkdir(@control_path) if !File.directory?(@control_path)
@@ -219,5 +189,72 @@ class FPM::Package::Deb < FPM::Package
       return File.join(@control_path, path)
     end
   end # def control_path
+
+  def write_control_tarball
+    # Use custom Debian control file when given ...
+    FileUtils.mkdir(control_path)
+
+    write_control # write the control file
+    write_scripts # write the maintainer scripts
+    write_conffiles # write the conffiles
+    write_debconf # write the debconf files
+
+    # Make the control.tar.gz
+    with(build_path("control.tar.gz")) do |controltar|
+      @logger.info("Creating", :path => controltar, :from => control_path)
+      safesystem(tar_cmd, "--numeric-owner", "--owner=0", "--group=0", "-zcf",
+                 controltar, "-C", control_path, ".")
+    end
+
+    @logger.debug("Removing no longer needed control dir", :path => control_path)
+  ensure
+    FileUtils.rm_r(control_path)
+  end # def write_control_tarball
+
+  def write_control
+    # Write the control file
+    with(control_path("control")) do |control|
+      if attributes["deb-custom-control"] 
+        @logger.debug("Using '#{attributes["deb-custom-control"]}' template for the control file")
+        control_data = File.read(attributes["deb-custom-control"])
+      else
+        @logger.debug("Using 'deb.erb' template for the control file")
+        control_data = template("deb.erb").result(binding)
+      end
+
+      @logger.debug("Writing control file", :path => control)
+      File.write(control, control_data)
+    end
+  end # def write_control
+
+  def write_scripts
+    SCRIPT_MAP.each do |script, filename|
+      with(control_path(filename)) do |path|
+        FileUtils.cp(scripts[script], filename)
+        # deb maintainer scripts are required to be executable
+        File.chmod(0755, filename)
+      end
+    end 
+  end # def write_scripts
+
+  def write_conffiles
+    File.new(control_path("conffiles")) do |out|
+      # 'config_files' comes from FPM::Package and is usually set with
+      # FPM::Command's --config-files flag
+      config_files.each { |cf| out.puts(cf) }
+    end
+  end # def write_conffiles
+
+  def write_debconf
+    if attributes[:deb_config]
+      FileUtils.cp(attributes[:deb_config], control_path("config"))
+      File.chmod(0755, control_path("config"))
+    end
+
+    if attributes[:deb_templates]
+      FileUtils.cp(attributes[:deb_templates], control_path("templates"))
+      File.chmod(0755, control_path("templates"))
+    end
+  end # def write_debconf
   public(:input, :output)
 end # class FPM::Target::Deb
