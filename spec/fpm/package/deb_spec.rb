d1edff085358420c29bebe4480d8129f9b2d66dc
Jordan Sissel <jls@semicomplete.com>
2012-03-08 15:21:38 -0800

    - Add dependency parsing from deb packages on input - specs passing

--- spec/fpm/package/deb_spec.rb
+++ spec/fpm/package/deb_spec.rb
@@ -4,35 +4,56 @@ require "fpm/package/deb" # local
 require "fpm/package/dir" # local
 
 describe FPM::Package::Deb do
-  subject { FPM::Package::Deb.new }
-
   describe "#output" do
-    context "package attributes" do
-      before :all do
-        tmpfile = Tempfile.new("fpm-test-deb")
-        @target = tmpfile.path
-        # The target file must not exist.
-        tmpfile.unlink
+    before :all do
+      # output a package, use it as the input, set the subject to that input
+      # package. This helps ensure that we can write and read packages
+      # properly.
+      tmpfile = Tempfile.new("fpm-test-deb")
+      target = tmpfile.path
+      # The target file must not exist.
+      tmpfile.unlink
 
-        File.write("/tmp/x", { :subject => subject }.inspect)
-        subject.name = "name"
-        subject.version = "123"
-        subject.iteration = "100"
-        subject.epoch = "5"
-        subject.dependencies << "something > 10"
-        subject.dependencies << "hello >= 20"
-        subject.output(@target)
+      @original = FPM::Package::Deb.new
+      @original.name = "name"
+      @original.version = "123"
+      @original.iteration = "100"
+      @original.epoch = "5"
+      @original.dependencies << "something > 10"
+      @original.dependencies << "hello >= 20"
+      @original.output(target)
+
+      @input = FPM::Package::Deb.new
+      @input.input(target)
+    end
+
+    after :all do
+      @original.cleanup
+      @input.cleanup
+    end # after
+
+    context "package attributes" do
+      it "should have the correct name" do
+        insist { @input.name } == @original.name
       end
 
-      after :all do
-        subject.cleanup
-      end # after
+      it "should have the correct version" do
+        insist { @input.version } == @original.version
+      end
 
-      it "should have the correct name"
-      it "should have the correct version"
-      it "should have the correct iteration"
-      it "should have the correct epoch"
-      it "should output a package with the correct dependencies"
+      it "should have the correct iteration" do
+        insist { @input.iteration } == @original.iteration
+      end
+
+      it "should have the correct epoch" do
+        insist { @input.epoch } == @original.epoch
+      end
+
+      it "should have the correct dependencies" do
+        @original.dependencies.each do |dep|
+          insist { @input.dependencies }.include?(dep)
+        end
+      end
     end
   end
-end # describe FPM::Package::RPM
+end # describe FPM::Package::Deb
