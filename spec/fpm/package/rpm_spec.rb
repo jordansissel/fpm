a40c5d1e66a451a2d363bea7cd8733ee724b22fc
Jordan Sissel <jls@semicomplete.com>
2012-03-08 10:17:36 -0800

    make sure some rpm spec stuff is correct

--- spec/fpm/package/rpm_spec.rb
+++ spec/fpm/package/rpm_spec.rb
@@ -2,36 +2,61 @@ require "spec_setup"
 require "fpm" # local
 require "fpm/package/rpm" # local
 require "fpm/package/dir" # local
-require "rpm" # gem arr-pm
+require "arr-pm/file" # gem 'arr-pm'
 
 describe FPM::Package::RPM do
   subject { FPM::Package::RPM.new }
 
-  before :each do
-    @target = Tempfile.new("fpm-test-rpm")
-  end # before
-
-  after :each do
-    subject.cleanup
-    @target.close
-  end # after
-
   describe "#output" do
-    context "basics" do
-      before :each do
+    context "package attributes" do
+      before :all do
+        @target = Tempfile.new("fpm-test-rpm")
         subject.name = "name"
-      end
-
-      it "should output a package with the correct name" do
+        subject.version = "123"
+        subject.iteration = "100"
+        subject.epoch = "5"
+        subject.dependencies << "something > 10"
+        subject.dependencies << "hello >= 20"
         subject.output(@target.path)
-        rpm = RPM::File.new(@target.path)
-        # TODO(sissel): verify rpm name vs subject.name
+        @rpm = ::RPM::File.new(@target.path)
+
+        @rpmtags = {}
+        @rpm.header.tags.each do |tag|
+          @rpmtags[tag.tag] = tag.value
+        end
       end
 
-      it "should output a package with the correct version"
-      it "should output a package with the correct iteration"
-      it "should output a package with the correct epoch"
-      it "should output a package with the correct dependencies"
+      after :all do
+        subject.cleanup
+        @target.close
+        @target.delete
+      end # after
+
+      it "should have the correct name" do
+        insist { @rpmtags[:name] } == subject.name
+      end
+
+      it "should have the correct version" do
+        insist { @rpmtags[:version] } == subject.version
+      end
+
+      it "should have the correct iteration" do
+        insist { @rpmtags[:release] } == subject.iteration
+      end
+
+      it "should have the correct epoch" do
+        insist { @rpmtags[:epoch].first.to_s } == subject.epoch
+      end
+
+      it "should output a package with the correct dependencies" do
+        # @rpm.requires is an array of [name, op, requires] elements
+        # fpm uses strings here, so convert.
+        requires = @rpm.requires.collect { |a| a.join(" ") }
+
+        subject.dependencies.each do |dep|
+          insist { requires }.include?(dep)
+        end
+      end
     end
   end
 end # describe FPM::Package::RPM
