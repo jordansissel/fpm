d2ca3db26d388ca43e5ece8c6b56079c823c1cae
Jordan Sissel <jls@semicomplete.com>
2012-03-09 20:00:07 +0000

    - skip rpm tests and warn if 'rpmbuild' is not found. - add an 'rspec' specific log channel

--- spec/spec_setup.rb
+++ spec/spec_setup.rb
@@ -1,16 +1,25 @@
 require "rubygems" # for ruby 1.8
-require "insist" # gem 'insist'
-require "rush" # gem 'rush'
+require "insist" # gem "insist"
+require "rush" # gem "rush"
+require "cabin" # gem "cabin"
 require "tmpdir" # stdlib
 require "tempfile" # stdlib
 require "fileutils" # stdlib
 
-# put 'lib' in RUBYLIB
+# put "lib" in RUBYLIB
 $: << File.join(File.dirname(File.dirname(__FILE__)), "lib")
+
+# for method "program_in_path?" etc
+require "fpm/util"
+include FPM::Util
 
 # Enable debug logs if requested.
 if $DEBUG or ENV["DEBUG"]
   Cabin::Channel.get.level = :debug
   Cabin::Channel.get.subscribe(STDOUT)
 end
+
+spec_logger = Cabin::Channel.get("rspec")
+spec_logger.subscribe(STDOUT)
+spec_logger.level = :warn
 
