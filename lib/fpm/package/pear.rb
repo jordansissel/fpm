b6fdfa96df63f48c33a212e87b481da1884656d3
Jordan Sissel <jls@semicomplete.com>
2012-03-08 17:21:47 -0800

    - move pear package type to new api

--- lib/fpm/package/pear.rb
+++ lib/fpm/package/pear.rb
@@ -4,38 +4,26 @@ require "fileutils"
 require "fpm/util"
 
 class FPM::Package::PEAR < FPM::Package
-  def self.flags(opts, settings)
-    opts.on("--package-prefix PREFIX",
-            "Prefix for PEAR packages") do |package_prefix|
-      settings.source[:package_prefix] = package_prefix
+  option "--package-prefix", "PREFIX",
+    "Name prefix for python package", :default => "php-pear"
+
+  def input(input_package)
+    if !program_in_path?("pear")
+      raise ExecutableNotFound.new("pear")
     end
-  end # def flags
 
-  def get_metadata
-    @pear_package = @paths.first
-    pear_cmd = "pear remote-info #{@pear_package}"
-    self[:name] = %x{#{pear_cmd} | sed -ne '/^Package\s*/s/^Package\s*//p'}.chomp
-    self[:version] = %x{#{pear_cmd} | sed -ne '/^Latest\s*/s/^Latest\s*//p'}.chomp
-    self[:summary] = %x{#{pear_cmd} | sed -ne '/^Summary\s*/s/^Summary\s*//p'}.chomp
-    if self[:settings][:package_prefix]
-      self[:package_prefix] = self[:settings][:package_prefix]
-    else
-      self[:package_prefix] = "php-pear"
+    pear_cmd = "pear remote-info #{input_package}"
+    name = %x{#{pear_cmd} | sed -ne '/^Package\s*/s/^Package\s*//p'}.chomp
+    self.name = "#{attributes[:pear_package_prefix]}-#{name}"
+    self.version = %x{#{pear_cmd} | sed -ne '/^Latest\s*/s/^Latest\s*//p'}.chomp
+    self.description  = %x{#{pear_cmd} | sed -ne '/^Summary\s*/s/^Summary\s*//p'}.chomp
+
+    safesystem("pear", "install", "-n", "-f", "-P", staging_path, input_package)
+ 
+    # Remove the stuff we don't want
+    delete_these = [".depdb", ".depdblock", ".filemap", ".lock", ".channel"]
+    Find.find(build_path) do |path|
+      FileUtils.rm_r(path) if delete_these.include?(File.basename(path))
     end
-	self[:name] = "#{self[:package_prefix]}-#{self[:name]}"
-  end # def get_metadata
-
-  def make_tarball!(tar_path, builddir)
-    tmpdir = "#{tar_path}.dir"
-    ::Dir.mkdir(tmpdir)
-    safesystem("pear install -n -f -P #{tmpdir} #{@pear_package}")
-	# Remove the stuff we don't want
-	['.depdb', '.depdblock', '.filemap', '.lock'].each { |f| safesystem("find #{tmpdir} -type f -name '#{f}' -exec rm {} \\;") }
-	# find exits non-zero even though it works, so we have to work around that
-	safesystem("find #{tmpdir} -type d -name '.channel*' -exec rm -rf {} \\; 2>/dev/null; exit 0")
-    tar(tar_path, '.', tmpdir)
-    @paths = %x{find #{tmpdir} -mindepth 1 -maxdepth 1 -type d | sed -e 's:^#{tmpdir}:.:'}.split("\n")
-    safesystem(*["gzip", "-f", tar_path])
-  end
-
+  end # def input
 end # class FPM::Package::PEAR
