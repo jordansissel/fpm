911046998747bd24e70663319ee5360dda273d08
Jordan Sissel <jls@semicomplete.com>
2012-03-15 00:17:13 +0000

    - Add specs for python

---
+++ spec/fixtures/python/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name="example",
+      version="1.0",
+      description="sample description",
+      author="sample author",
+      author_email="sample email",
+      url="sample url",
+      packages=[],
+      package_dir={},
+      requires=["dependency1", "dependency2"],
+      )
+
