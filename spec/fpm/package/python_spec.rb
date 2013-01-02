9a4744b9d6e9ebe1968eee391444e6857212c9e2
Jay Buffington <jaybuff@apple.com>
2013-01-02 14:47:15 -0800

    control case of package name and its dependencies
    Added the following two flags:
      --[no-]python-downcase-name
      (python only) Should the target package name be in lowercase? (default: true)
      --[no-]python-downcase-dependencies
      (python only) Should the package dependencies be in lowercase? (default: true)
    
    fixes #326

--- spec/fpm/package/python_spec.rb
+++ spec/fpm/package/python_spec.rb
@@ -19,37 +19,54 @@ describe FPM::Package::Python, :if => python_usable? do
   after :each do
     subject.cleanup
   end
-  
-  context "when :python_fix_name? is true" do
+
+  context "when :python_downcase_name? is false" do
     before :each do
-      subject.attributes[:python_fix_name?] = true
+      subject.attributes[:python_downcase_name?] = false
     end
-
-    context "and :python_package_name_prefix is nil/default" do
-      it "should prefix the package with 'python-'" do
-        subject.input(example_dir)
-        insist { subject.name } == "python-example"
-      end
-    end
-
-    context "and :python_package_name_prefix is set" do
-      it "should prefix the package name appropriately" do
-        prefix = "whoa"
-        subject.attributes[:python_package_name_prefix] = prefix
-        subject.input(example_dir)
-        insist { subject.name } == "#{prefix}-example"
-      end
+    
+    it "should leave the package name as is" do
+      subject.input(example_dir)
+      insist { subject.name } == "Example"
     end
   end
 
-  context "when :python_fix_name? is false" do
+  context "when :python_downcase_name? is true" do
     before :each do
-      subject.attributes[:python_fix_name?] = false
+      subject.attributes[:python_downcase_name?] = true
     end
 
-    it "it should not prefix the name at all" do
-      subject.input(example_dir)
-      insist { subject.name } == "example"
+    context "when :python_fix_name? is true" do
+      before :each do
+        subject.attributes[:python_fix_name?] = true
+      end
+
+      context "and :python_package_name_prefix is nil/default" do
+        it "should prefix the package with 'python-'" do
+          subject.input(example_dir)
+          insist { subject.name } == "python-example"
+        end
+      end
+
+      context "and :python_package_name_prefix is set" do
+        it "should prefix the package name appropriately" do
+          prefix = "whoa"
+          subject.attributes[:python_package_name_prefix] = prefix
+          subject.input(example_dir)
+          insist { subject.name } == "#{prefix}-example"
+        end
+      end
+    end
+
+    context "when :python_fix_name? is false" do
+      before :each do
+        subject.attributes[:python_fix_name?] = false
+      end
+
+      it "it should not prefix the name at all" do
+        subject.input(example_dir)
+        insist { subject.name } == "example"
+      end
     end
   end
 end # describe FPM::Package::Python
