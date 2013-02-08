e93c4fae124f896b32dc7f75a612df8f07affc1e
Zsolt Takács <zsolt@takacs.cc>
2013-02-08 20:56:30 +0100

    pear: add options for bin_dir, php_bin, and php_dir settings
    packages with executables will put their bins in bin_dir, which is
    prefix/pear by default. php_bin can be used if the php interpreter is
    in a different location on the target system. php_dir can be used for
    overriding the directory where php files are put (useful on debian).

--- lib/fpm/package/pear.rb
+++ lib/fpm/package/pear.rb
@@ -13,8 +13,17 @@ class FPM::Package::PEAR < FPM::Package
   option "--channel", "CHANNEL_URL",
     "The pear channel url to use instead of the default."
 
-  option "--channel-update", :flag, 
+  option "--channel-update", :flag,
     "call 'pear channel-update' prior to installation"
+
+  option "--bin-dir", "BIN_DIR",
+    "Directory to put binaries in"
+
+  option "--php-bin", "PHP_BIN",
+    "Specify php executable path if differs from the os used for packaging"
+
+  option "--php-dir", "PHP_DIR",
+    "Specify php dir relative to prefix if differs from pear default (pear/php)"
 
   # Input a PEAR package.
   #
@@ -22,7 +31,7 @@ class FPM::Package::PEAR < FPM::Package
   #
   # Attributes that affect behavior here:
   # * :prefix - changes the install root, default is /usr/share
-  # * :pear_package_name_prefix - changes the 
+  # * :pear_package_name_prefix - changes the
   def input(input_package)
     if !program_in_path?("pear")
       raise ExecutableNotFound.new("pear")
@@ -33,6 +42,19 @@ class FPM::Package::PEAR < FPM::Package
     config = File.expand_path(build_path("pear.config"))
     installroot = attributes[:prefix] || "/usr/share"
     safesystem("pear", "config-create", staging_path(installroot), config)
+
+    if attributes[:pear_php_dir]
+      @logger.info("Setting php_dir", :php_dir => attributes[:pear_php_dir])
+      safesystem("pear", "-c", config, "config-set", "php_dir", "#{staging_path(installroot)}/#{attributes[:pear_php_dir]}")
+    end
+
+    bin_dir = attributes[:pear_bin_dir] || "usr/bin"
+    @logger.info("Setting bin_dir", :bin_dir => bin_dir)
+    safesystem("pear", "-c", config, "config-set", "bin_dir", bin_dir)
+
+    php_bin = attributes[:pear_php_bin] || "/usr/bin/php"
+    @logger.info("Setting php_bin", :php_bin => php_bin)
+    safesystem("pear", "-c", config, "config-set", "php_bin", php_bin)
 
     # try channel-discover
     if !attributes[:pear_channel].nil?
