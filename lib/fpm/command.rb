0ef743c8dc57a97e7e06a2836cc53fc7bffadb06
Pranay Kanwar <pranay.kanwar@gmail.com>
2013-03-22 08:16:33 +0000

    Add --no-auto-depends flag

--- lib/fpm/command.rb
+++ lib/fpm/command.rb
@@ -87,6 +87,9 @@ class FPM::Command < Clamp::Command
   option "--no-depends", :flag, "Do not list any dependencies in this package",
     :default => false
 
+  option "--no-auto-depends", :flag, "Do not list any dependencies in this package automatically",
+    :default => false
+
   option "--provides", "PROVIDES",
     "What this package provides (usually a name). This flag can be "\
     "specified multiple times." do |val|
