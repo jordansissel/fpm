6b15d553e23aab9806180ea7e042ab79621e4dab
Jordan Sissel <jls@semicomplete.com>
2013-04-09 14:16:07 -0700

    - use flags, not npmrc. This let's me skip the weird HOME hackery   I was doing before. - now should abort if 'npm ls' fails.

--- lib/fpm/package/npm.rb
+++ lib/fpm/package/npm.rb
@@ -17,25 +17,26 @@ class FPM::Package::NPM < FPM::Package
     # * npm respects PREFIX
     settings = {
       "cache" => build_path("npm_cache"),
-      "loglevel" => "silent"
+      "loglevel" => "warn",
+      "global" => "true"
     }
 
     if attributes.include?(:prefix) && !attributes[:prefix].nil?
       settings["prefix"] = staging_path(attributes[:prefix])
     else
       @logger.info("Setting default npm install prefix",
-                   :prefix => "/usr/local/lib")
-      settings["prefix"] = staging_path("/usr/local/lib")
+                   :prefix => "/usr/local")
+      settings["prefix"] = staging_path("/usr/local")
     end
 
-    fakehome = build_path("home")
-    ::Dir.mkdir(fakehome) if !::Dir.exists?(fakehome)
+    FileUtils.mkdir_p(settings["prefix"])
 
-    original_home = ENV["HOME"]
-    ENV["HOME"] = fakehome
+    npm_flags = []
     settings.each do |key, value|
+      # npm lets you specify settings in a .npmrc but the same key=value settings
+      # are valid as '--key value' command arguments to npm. Woo!
       @logger.debug("Configuring npm", key => value)
-      safesystem(attributes[:npm_bin], "config", "set", key, value)
+      npm_flags += [ "--#{key}", value ]
     end
 
     install_args = [
@@ -47,13 +48,24 @@ class FPM::Package::NPM < FPM::Package
 
     # The only way to get npm to respect the 'prefix' setting appears to
     # be to set the --global flag.
-    install_args << "--global"
+    #install_args << "--global"
+    install_args += npm_flags
 
     safesystem(*install_args)
 
-    # Query 
-    npm_ls = JSON.parse(`#{attributes[:npm_bin]} ls --json --long #{package}`)
-    p npm_ls
+    # Query details about our now-installed package.
+    # We do this by using 'npm ls' with json + long enabled to query details
+    # about the installed package.
+    npm_ls_cmd = [attributes[:npm_bin], "ls", "--json", "--long", package] + npm_flags
+    npm_ls_fd = IO.popen(npm_ls_cmd)
+    npm_ls_out = npm_ls_fd.read()
+    pid, status = Process.waitpid2(npm_ls_fd.pid)
+    if !status.success?
+      raise FPM::Util::ProcessFailed.new("#{npm_ls_cmd.first} failed (exit " \
+                                         "code #{status.exitstatus}). " \
+                                         "Full command was: #{npm_ls_cmd.inspect}")
+    end
+    npm_ls = JSON.parse(npm_ls_out)
     name, info = npm_ls["dependencies"].first
 
     self.name = [attributes[:npm_package_name_prefix], name].join("-")
@@ -75,13 +87,11 @@ class FPM::Package::NPM < FPM::Package
     #
     # To that end, I don't think we necessarily need to include 
     # any automatic dependency information since every 'npm install'
-    # is fully self-contained.
+    # is fully self-contained. That's why you don't see any bother, yet,
+    # to include the package's dependencies in here.
     #
     # It's possible someone will want to decouple that in the future,
     # but I will wait for that feature request.
-
-    # Restore the original HOME setting
-    ENV["HOME"] = original_home 
   end
 
   public(:input)
