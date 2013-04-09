745ebbeb07a7e3700104a46406c8a716ae5f50bb
Jordan Sissel <jls@semicomplete.com>
2013-04-09 12:55:03 -0700

    Add work-in-progress for npm support

--- lib/fpm/package/npm.rb
+++ lib/fpm/package/npm.rb
@@ -4,33 +4,85 @@ require "fpm/util"
 require "fileutils"
 
 class FPM::Package::NPM < FPM::Package
-  def get_source(params)
-    @npm = @paths.first
-  end # def get_source
+  # Flags '--foo' will be accessable  as attributes[:npm_foo]
+  option "--bin", "NPM_EXECUTABLE",
+    "The path to the npm executable you wish to run.", :default => "npm"
 
-  def download(npm_name, version=nil)
-  end # def download
+  option "--package-name-prefix", "PREFIX", "Name to prefix the package " \
+    "name with.", :default => "node"
 
-  def get_metadata
-    # set self[:...] values
-    # :name
-    # :maintainer
-    # :url
-    # :category
-    # :dependencies
-  end # def get_metadata
+  private
+  def input(package)
+    # Notes:
+    # * npm respects PREFIX
+    settings = {
+      "cache" => build_path("npm_cache"),
+      "loglevel" => "silent"
+    }
 
-  def make_tarball!(tar_path, builddir)
-    tmpdir = "#{tar_path}.dir"
-    installdir = "#{tmpdir}/#{::Gem::dir}"
-    ::FileUtils.mkdir_p(installdir)
-    args = ["gem", "install", "--quiet", "--no-ri", "--no-rdoc",
-       "--install-dir", installdir, "--ignore-dependencies", @paths.first]
-    safesystem(*args)
-    tar(tar_path, ".", tmpdir)
+    if attributes.include?(:prefix) && !attributes[:prefix].nil?
+      settings["prefix"] = staging_path(attributes[:prefix])
+    else
+      @logger.info("Setting default npm install prefix",
+                   :prefix => "/usr/local/lib")
+      settings["prefix"] = staging_path("/usr/local/lib")
+    end
 
-    # TODO(sissel): Make a helper method.
-    safesystem(*["gzip", "-f", tar_path])
+    fakehome = build_path("home")
+    ::Dir.mkdir(fakehome) if !::Dir.exists?(fakehome)
+
+    original_home = ENV["HOME"]
+    ENV["HOME"] = fakehome
+    settings.each do |key, value|
+      @logger.debug("Configuring npm", key => value)
+      safesystem(attributes[:npm_bin], "config", "set", key, value)
+    end
+
+    install_args = [
+      attributes[:npm_bin],
+      "install",
+      # use 'package' or 'package@version'
+     (version ? "#{package}@#{version}" : package)
+    ]
+
+    # The only way to get npm to respect the 'prefix' setting appears to
+    # be to set the --global flag.
+    install_args << "--global"
+
+    safesystem(*install_args)
+
+    # Query 
+    npm_ls = JSON.parse(`#{attributes[:npm_bin]} ls --json --long #{package}`)
+    p npm_ls
+    name, info = npm_ls["dependencies"].first
+
+    self.name = [attributes[:npm_package_name_prefix], name].join("-")
+    self.version = info["version"]
+
+    if info.include?("repository")
+      self.url = info["repository"]["url"]
+    else
+      self.url = "https://npmjs.org/package/#{self.name}"
+    end
+
+    self.description = info["description"]
+    self.vendor = sprintf("%s <%s>", info["author"]["name"],
+                          info["author"]["email"])
+
+    # npm installs dependencies in the module itself, so if you do
+    # 'npm install express' it installs dependencies (like 'connect')
+    # to: node_modules/express/node_modules/connect/...
+    #
+    # To that end, I don't think we necessarily need to include 
+    # any automatic dependency information since every 'npm install'
+    # is fully self-contained.
+    #
+    # It's possible someone will want to decouple that in the future,
+    # but I will wait for that feature request.
+
+    # Restore the original HOME setting
+    ENV["HOME"] = original_home 
   end
 
+  public(:input)
 end # class FPM::Package::NPM
