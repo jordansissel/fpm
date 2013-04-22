612a2754a6e6d02609275a7d51bc4e4e0485eac7
Jordan Sissel <jls@semicomplete.com>
2013-04-22 16:38:02 -0700

    Update gem-to-rpm.rb

--- examples/api/gem-to-rpm.rb
+++ examples/api/gem-to-rpm.rb
@@ -2,6 +2,10 @@ $: << File.join(File.dirname(__FILE__), "..", "..", "lib")
 require "fpm"
 
 package = FPM::Package::Gem.new
+
+# the Gem package takes a string name of the package to download/install.
+# Example, run this script with 'rails' as an argument and it will convert
+# the latest 'rails' gem into rpm. 
 package.input(ARGV[0])
 rpm = package.convert(FPM::Package::RPM)
 begin
