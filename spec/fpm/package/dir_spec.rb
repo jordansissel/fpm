8360506c394b18e568d17b66e650639b46fbae7b
Jordan Sissel <jls@semicomplete.com>
2013-07-11 22:23:29 -0700

    - Add path mapping   Behavior     * some/file=/some/path       Result: /some/path     * some/directory=/some/path       Result: /some/path/directory/... - drop dependency on Rush

--- spec/fpm/package/dir_spec.rb
+++ spec/fpm/package/dir_spec.rb
@@ -1,87 +1,99 @@
 require "spec_setup"
 require "fpm" # local
 require "fpm/package/dir" # local
+require "stud/temporary"
 
 describe FPM::Package::Dir do
-  before :each do
-    @source = FPM::Package::Dir.new
-    @rush = Rush::Box.new("localhost")
-    @tmpdir = @rush[::Dir.mktmpdir("package-test-tmpdir")]
-    @output = @rush[::Dir.mktmpdir("package-test-output")]
-  end # before
+  #let(:tmpdir) { Stud::Temporary.directory("tmpdir") }
+  #let(:output) { Stud::Temporary.directory("output") }
+  let(:tmpdir) { ::Dir.mkdir("/tmp/tmpdir"); "/tmp/tmpdir" }
+  let(:output) { ::Dir.mkdir("/tmp/output"); "/tmp/output" }
 
   after :each do
-    @source.cleanup
-    FileUtils.rm_r(@tmpdir.full_path)
-    FileUtils.rm_r(@output.full_path)
+    subject.cleanup
+    FileUtils.rm_r(tmpdir)
+    FileUtils.rm_r(output)
   end # after
 
   it "single file: should copy single files to the root of the output" do
-    file = @tmpdir["hello"]
-    file.write "Hello world"
-    @source.input(@tmpdir.full_path)
-    @source.output(@output.full_path)
-
-    path = File.join(".", file.full_path)
-    insist { @output[path].contents } == file.contents
-    #assert_equal(@output[File.join(".", file.full_path)].contents,
-                 #file.contents, "The file #{@tmpdir["hello"].full_path} should appear in the output")
+    hello_in = File.join(tmpdir, "hello")
+    hello_out = File.join(output, tmpdir, "hello")
+    file = File.write(hello_in, "Hello world")
+    subject.input(tmpdir)
+    subject.output(output)
+    insist { File.read(hello_out) } == File.read(hello_in)
   end
 
   it "should copy the full path given (single file)" do
-    dir = @tmpdir.create_dir("a/b/c")
-    file = dir.create_file("hello")
-    file.write "Hello world"
-    @source.input(@tmpdir.full_path)
-    @source.output(@output.full_path)
-
-    path = File.join(".", file.full_path)
-    insist { @output[path].contents } == file.contents
-    #assert_equal(@output[File.join(".", file.full_path)].contents,
-                 #file.contents, "The file #{@tmpdir["a/b/c/hello"].full_path} should appear in the output")
+    dir = File.join(tmpdir, "a", "b", "c")
+    FileUtils.mkdir_p(dir)
+    hello_in = File.join(dir, "hello")
+    hello_out = File.join(output, dir, "hello")
+    File.write(hello_in, "Hello world")
+    subject.input(tmpdir)
+    subject.output(output)
+    insist { File.read(hello_out) } == File.read(hello_in)
   end
 
   it "should copy entire directories given as input" do
-    dir = @tmpdir.create_dir("a/b/c")
+    dir = File.join(tmpdir, "a", "b", "c")
+    FileUtils.mkdir_p(dir)
     files = rand(50).times.collect do |i|
-      dir.create_file("hello-#{i}")
+      file = File.join(dir, "hello-#{i}")
+      File.write(file, rand(1000))
+      next file
     end
-    files.each { |f| f.write(rand(1000)) }
 
-    @source.input(@tmpdir.full_path)
-    @source.output(@output.full_path)
+    subject.input(tmpdir)
+    subject.output(output)
 
     files.each do |file|
-      path = File.join(".", file.full_path)
-      insist { @output[path].contents } == file.contents
-                   #file.contents, "The file #{file.full_path} should appear in the output")
+      insist { File.read(File.join(output, file)) } == File.read(file)
     end
   end
 
   it "should obey the :prefix attribute" do
-    prefix = @source.attributes[:prefix] = "/usr/local"
-    file = @tmpdir["hello"]
-    file.write "Hello world"
-    @source.input(@tmpdir.full_path)
-    @source.output(@output.full_path)
+    prefix = subject.attributes[:prefix] = "/usr/local"
+    file = File.join(tmpdir, "hello")
+    File.write(file, "Hello world")
+    subject.input(tmpdir)
+    subject.output(output)
 
-    expected_path = File.join(".", file.full_path)
+    expected_path = File.join(".", file)
     path = File.join(prefix, expected_path)
-    insist { @output[path].contents } == file.contents
-                 #file.contents, "The file #{@tmpdir["hello"].full_path} should appear in the output")
+    insist { File.read(File.join(output, path)) } == File.read(file)
   end
 
   it "should obey :chdir and :prefix attributes together" do
-    prefix = @source.attributes[:prefix] = "/usr/local"
-    chdir = @source.attributes[:chdir] = @tmpdir.full_path
-    file = @tmpdir["hello"]
-    file.write "Hello world"
-    @source.input(".") # since we chdir, copy the entire root
-    @source.output(@output.full_path)
+    prefix = subject.attributes[:prefix] = "/usr/local"
+    chdir = subject.attributes[:chdir] = tmpdir
+    file = File.join(tmpdir, "hello")
+    File.write(file, "Hello world")
+    subject.input(".") # since we chdir, copy the entire root
+    subject.output(output)
 
     # path relative to the @output directory.
-    expected_path = File.join(".", prefix, file.name)
-    insist { @output[expected_path].contents } == file.contents
-                 #file.contents, "The file #{@tmpdir["hello"].full_path} should appear in the output")
+    expected_path = File.join(".", prefix, File.basename(file))
+    insist { File.read(File.join(output, expected_path)) } == File.read(file)
+  end
+
+  context "path mapping" do
+    it "should map a file -> directory" do
+      file = File.join(tmpdir, "hello")
+      File.write(file, "Hello world")
+      subject.input("#{tmpdir}=/usr/local")
+      subject.output(output)
+      insist { File }.exists?(File.join(output, "/usr/local", "hello"))
+    end
+
+    it "should map a directory -> directory" do
+      file = File.join(tmpdir, "example", "hello")
+      dir = File.dirname(file)
+      ::Dir.mkdir(dir)
+      File.write(file, "Hello world")
+      subject.input("#{tmpdir}=/usr/local")
+      subject.output(output)
+      insist { File }.exists?(File.join(output, "/usr/local", "example", "hello"))
+    end
   end
 end # describe FPM::Package::Dir
