9a101386c932c23e060ff723bfd243eb72713f1d
Pranay Kanwar <pranay.kanwar@gmail.com>
2013-09-19 15:55:57 +0530

    Fixes #554 do not map existing paths with = in them

--- lib/fpm/package/dir.rb
+++ lib/fpm/package/dir.rb
@@ -43,7 +43,7 @@ class FPM::Package::Dir < FPM::Package
     # This mapping should work the same way 'rsync -a' does
     #   Meaning 'rsync -a source dest'
     #   and 'source=dest' in fpm work the same as the above rsync
-    if path =~ /.=./
+    if path =~ /.=./ && !File.exists?(chdir == '.' ? path : File.join(chdir, path))
       origin, destination = path.split("=", 2)
 
       if File.directory?(origin) && origin[-1,1] == "/"
