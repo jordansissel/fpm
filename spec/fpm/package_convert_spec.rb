17d38f22de68bc5faeb9641997188c3a723decf8
Jordan Sissel <jls@semicomplete.com>
2013-12-05 22:40:59 -0800

    - fix whitespace

--- spec/fpm/package_convert_spec.rb
+++ spec/fpm/package_convert_spec.rb
@@ -3,20 +3,20 @@ require "fpm" # local
 
 describe "FPM::Package#convert" do
 
-    let(:gem_package_name_prefix) { 'rubygem19' }
-    let(:default_rpm_compression) { 'gzip' }
+  let(:gem_package_name_prefix) { 'rubygem19' }
+  let(:default_rpm_compression) { 'gzip' }
 
-    subject do
-      source = FPM::Package::Gem.new
-      source.attributes[:gem_package_name_prefix ] = 'rubygem19'
-      source.convert(FPM::Package::RPM)
-    end
+  subject do
+    source = FPM::Package::Gem.new
+    source.attributes[:gem_package_name_prefix ] = 'rubygem19'
+    source.convert(FPM::Package::RPM)
+  end
 
-    it "applies the default attributes for target format" do
-      insist { subject.attributes[:rpm_compression] } == default_rpm_compression
-    end
+  it "applies the default attributes for target format" do
+    insist { subject.attributes[:rpm_compression] } == default_rpm_compression
+  end
 
-    it "remembers attributes applied to source" do
-      insist { subject.attributes[:gem_package_name_prefix] } == gem_package_name_prefix
-    end
+  it "remembers attributes applied to source" do
+    insist { subject.attributes[:gem_package_name_prefix] } == gem_package_name_prefix
+  end
 end
