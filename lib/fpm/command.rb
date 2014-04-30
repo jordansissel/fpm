jordansissel/fpm

commit afb0dce244d4965d582d340c2ba46476da376113

Support being given a directory for the output path (-p flag)
This should fix #656 and maybe a few other things.

lib/fpm/command.rb
@@ -392,7 +392,15 @@ class FPM::Command < Clamp::Command
 
     # Write the output somewhere, package can be nil if no --package is specified, 
     # and that's OK.
-    package_file = output.to_s(package)
+    
+    # If the package output (-p flag) is a directory, write to the default file name
+    # but inside that directory.
+    if File.directory?(package)
+      package_file = File.join(package, output.to_s)
+    else
+      package_file = output.to_s(package)
+    end
+
     begin
       output.output(package_file)
     rescue FPM::Package::FileAlreadyExists => e

spec/fpm/command_spec.rb
@@ -1,4 +1,5 @@
 require "spec_setup"
+require "stud/temporary"
 require "fpm" # local
 require "fpm/command" # local
 
@@ -22,4 +23,19 @@ describe FPM::Command do
   describe "--config-files"
   describe "--directories"
   describe "-a | --architecture"
+
+  describe "-p | --package" do
+    context "when given a directory" do
+      it "should write the package to the given directory." do
+        Stud::Temporary.directory do |path|
+          cmd = FPM::Command.new("fpm")
+          cmd.run(["-s", "empty", "-t", "deb", "-n", "example", "-p", path])
+          files = Dir.new(path).to_a - [".", ".."]
+
+          insist { files.size } == 1
+          insist { files[0] } =~ /^example_/
+        end
+      end
+    end
+  end
 end

commit 0266115181ea2bd1f11e1dcba9adeeab27d56300

This fixes an issue introduced in afb0dce2
If no package name was given, fpm bailed out.

lib/fpm/command.rb
@@ -395,7 +395,7 @@ class FPM::Command < Clamp::Command
     
     # If the package output (-p flag) is a directory, write to the default file name
     # but inside that directory.
-    if File.directory?(package)
+    if ! package.nil? && File.directory?(package)
       package_file = File.join(package, output.to_s)
     else
       package_file = output.to_s(package)