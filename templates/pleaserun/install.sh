e1336aafda457447fd510ead59dec6d0cb90c59c
Jordan Sissel <jls@semicomplete.com>
2016-05-14 17:35:02 -0700

    Make copying not rely on rsync. Also tell the user if no platform was detected.

--- templates/pleaserun/install.sh
+++ templates/pleaserun/install.sh
@@ -9,14 +9,38 @@ silent() {
 install_files() {
   # TODO(sissel): Need to know what prefix the files exist at
   platform="$1"
+  version="$(version_${platform})"
 
-  cp -Rp "${BASEDIR}/${platform}/files/" "/"
+  (
+    # TODO(sissel): Should I just rely on rsync for this stuff?
+    #rsync -av "${BASEDIR}/${platform}/${version}/files/" /
+    cd "${BASEDIR}/${platform}/${version}/files/" || exit 1
+    find . -print0 \
+      | xargs -0 -n1 sh -c 'd="${1##.}"; if [ ! -z "$d" ] ; then if [ -d "$1" -a ! -d "$d" ] ; then mkdir "$d"; fi; if [ -f "$1" ] ; then cp -p "$1" "$d" ; fi; fi' -
+  )
 }
 
 install_actions() {
   # TODO(sissel): Need to know what prefix the files exist at
   platform="$1"
-  . "${BASEDIR}/${platform}/activate.sh"
+  version="$(version_${platform})"
+  . "${BASEDIR}/${platform}/${version}/install_actions.sh"
+}
+
+version_systemd() {
+  echo default
+}
+
+version_launchd() {
+  echo 10.9
+}
+
+version_upstart() {
+  echo "default"
+}
+
+version_sysv() {
+  echo lsb-3.1
 }
 
 has_systemd() {
@@ -43,12 +67,19 @@ has_launchd() {
   [ -d "/Library/LaunchDaemons" ] && silent which launchtl
 }
 
-platforms="systemd upstart daemontools freebsd_rcng launchd sysv"
+platforms="systemd upstart launchd sysv"
+installed=0
 for platform in $platforms ; do
   if has_$platform ; then
-    echo "Platform $platform detected.."
+    version="$(version_$platform)"
+    echo "Platform $platform ($version) detected. Installing service."
     install_files $platform
     install_actions $platform
+    installed=1
     break
   fi
 done
+
+if [ "$installed" -eq 0 ] ; then
+  echo "Failed to detect any service platform, so no service was installed. Files are available in ${BASEDIR} if you need them."
+fi
