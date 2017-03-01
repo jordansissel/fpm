01b90f720147ce76070b388500129c3b9b62ca10
Syed Faraaz Ahmad <faraaz98@live.com>
2017-03-02 00:17:25 +0530

    Update fpm.rb
    Remove the explicit requiring of all the packages. Instead, add a small snippet to automatically require all the packages in the package directory.

--- lib/fpm.rb
+++ lib/fpm.rb
@@ -1,20 +1,8 @@
 require "fpm/namespace"
 
 require "fpm/package"
-require "fpm/package/dir"
-require "fpm/package/gem"
-require "fpm/package/deb"
-require "fpm/package/npm"
-require "fpm/package/rpm"
-require "fpm/package/tar"
-require "fpm/package/cpan"
-require "fpm/package/pear"
-require "fpm/package/empty"
-require "fpm/package/puppet"
-require "fpm/package/python"
-require "fpm/package/osxpkg"
-require "fpm/package/solaris"
-require "fpm/package/p5p"
-require "fpm/package/pkgin"
-require "fpm/package/freebsd"
-require "fpm/package/apk"
+
+packages_list = Dir.entries "fpm/package"
+packages_list.each do |package_file|
+  require "fpm/package/#{package_file.gsub(".rb", "")}"
+end
