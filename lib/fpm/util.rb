782f3edcec0597b73f9077e5f6a7ff3faecce469
Dan Kegel <dank@oblong.com>
2017-06-23 17:44:07 -0700

    Remove debug prints

--- lib/fpm/util.rb
+++ lib/fpm/util.rb
@@ -290,11 +290,9 @@ module FPM::Util
       ["--sort=name", "--mtime=@0"].each do |opt|
         system("#{tar} #{opt} -cf fpm-dummy.tar.tmp fpm-dummy.tmp > /dev/null 2>&1")
         if $?.exitstatus == 0
-          puts("tar_cmd: #{tar} #{opt} succeeded")
           opts << opt
           score += 1
         end
-        puts("tar_cmd: #{tar} #{opt} failed")
       end
       if score > bestscore
         best=tar
