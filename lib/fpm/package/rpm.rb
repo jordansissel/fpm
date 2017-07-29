a15f60e20f57b8cad814671860bfd3338f304583
Jordan Sissel <jls@semicomplete.com>
2017-07-29 01:19:10 -0700

    Restore prior --config-files behavior
    PR #1379/#860 introduced a new behavior for --config-files that fetches
    config files from the local filesystem. The prior behavior was to
    require these files to be provided by the source package (`--source`
    flag).
    
    This patch makes a hybrid solution which does, for any given
    `--config-files foo` flag:
    
    * does the path `foo` already exist in the staging area? Do nothing.
    This means the package source (`-s gem` for example) provided this file.
    * or, does the path `foo` exist in the local filesystem? If so, copy it
    into the staging area.
    * otherwise, consider this to be an error that the --config-files
    setting cannot find the file, and fpm will fail with an error.

--- lib/fpm/package/rpm.rb
+++ lib/fpm/package/rpm.rb
@@ -474,11 +474,18 @@ class FPM::Package::RPM < FPM::Package
 
     # include external config files
     (attributes[:config_files] or []).each do |conf|
-      path = conf
-      dest_conf = File.join(staging_path, path)
-      FileUtils.mkdir_p(File.dirname(dest_conf))
-      FileUtils.cp_r conf, dest_conf
-      File.chmod(0755, dest_conf)
+      dest_conf = File.join(staging_path, conf)
+
+      if File.exist?(dest_conf)
+        logger.debug("Using --config-file from staging area", :path => conf)
+      elsif File.exist?(conf)
+        logger.info("Copying --config-file from local path", :path => conf)
+        FileUtils.mkdir_p(File.dirname(dest_conf))
+        FileUtils.cp_r conf, dest_conf
+      else
+        logger.error("Failed to find given --config-file", :path => conf)
+        raise "Could not find config file '#{conf}' in staging area or on host. This can happen if you specify `--config-file '#{conf}'` but this file does not exist in the source package and also does not exist in filesystem."
+      end
     end
 
     # scan all conf file paths for files and add them
