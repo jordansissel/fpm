ca02692bcb8c3a4855e778c23bee8fb37079444d
Dave L <dave@lemonby.com>
2018-10-17 09:57:03 +1100

    Changed the mode of templates file for deb packages to 0644 (#1489)
    If "--deb-templates" is used, fpm uses the wrong mode for this file. Since it is not an executable file, it should
    not have the executable bits set. The lintian tool complains (with an error) that the mode is incorrect, and that
    it should not be executable. Changing the mode to 0644 resolves this.

--- lib/fpm/package/deb.rb
+++ lib/fpm/package/deb.rb
@@ -978,7 +978,7 @@ class FPM::Package::Deb < FPM::Package
 
     if attributes[:deb_templates]
       FileUtils.cp(attributes[:deb_templates], control_path("templates"))
-      File.chmod(0755, control_path("templates"))
+      File.chmod(0644, control_path("templates"))
     end
   end # def write_debconf
 
