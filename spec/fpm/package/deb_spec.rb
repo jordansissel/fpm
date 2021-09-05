jordansissel/fpm

commit 541550a74dbaa0123e1e3bdd5e85484dd8def65c

Allow versions in `Provides` field for Debian packages
This fixes #1788. This also reverts #280. For #280, at the time, this
change to ignore versions was correct. Two years after #280, Debian
began allowing `Provides` field to have versions.

This change also fixes bug in gem-to-deb conversion where previously an
incorrect Provides syntax would be generated (but thanks to #280,
removed), so this bug was only noticed after #280 was undone!

Computers are hard sometimes.

Added tests for gem-to-deb conversion specifically for the Provides
field.

Tested manually with Docker on Ubuntu 14.04 and 18.04 and results meet
expectations.

The history here is follows:
* In 2012, fpm was patched to remove version specifiers in Provides
  field because Debian didn't support it.
* In 2014, Debian dpkg[1] added support for versions in Provides field
* Somewhere between 2015-2018, Debian and Ubuntu included this new
  version of dpkg.
* Debian packaging policy docs (v4.4.0) was updated to allow versions
  in the Provides field.

Expected impacts:
* Older versions of dpkg/etc should _ignore_ the presence of a version
  specifier. Testing on Ubuntu 14.04 confirmed this.
* Newer versions of dpkg/etc should respect the presence of a version
  specifier. Testing on Ubuntu 18.04 confirmed this.

[1] https://launchpad.net/debian/+source/dpkg/1.17.11

lib/fpm/package/deb.rb
@@ -687,6 +687,19 @@ class FPM::Package::Deb < FPM::Package
         File.unlink(changelog_path)
       end
     end
+
+    if origin == FPM::Package::Gem
+      # fpm's gem input will have provides as "rubygem-name = version"
+      # and we need to convert this to Debian-style "rubygem-name (= version)"
+      self.provides = self.provides.collect do |provides|
+        m = /^(#{attributes[:gem_package_name_prefix]})-([^\s]+)\s*=\s*(.*)$/.match(provides)
+        if m
+          "#{m[1]}-#{m[2]} (= #{m[3]})"
+        else
+          provides
+        end
+      end
+    end
   end # def converted_from
 
   def debianize_op(op)
@@ -774,6 +787,11 @@ class FPM::Package::Deb < FPM::Package
       logger.warn("Replacing 'provides' underscores with dashes in '#{provides}' because " \
                    "debs don't like underscores")
       provides = provides.gsub("_", "-")
+    end
+
+    if m = provides.match(/^([A-Za-z0-9_-]+)\s*=\s*(\d+.*$)/)
+      logger.warn("Replacing 'provides' entry #{provides} with syntax 'name (= version)'")
+      provides = "#{m[1]} (= #{m[2]})"
     end
     return provides.rstrip
   end

spec/fpm/package/deb_spec.rb
@@ -143,7 +143,7 @@ describe FPM::Package::Deb do
       original.architecture = "all"
       original.dependencies << "something > 10"
       original.dependencies << "hello >= 20"
-      original.provides << "#{original.name} = #{original.version}"
+      original.provides << "#{original.name} (= #{original.version})"
 
       # Test to cover PR#591 (fix provides names)
       original.provides << "Some-SILLY_name"
@@ -234,11 +234,6 @@ describe FPM::Package::Deb do
         original.dependencies.each do |dep|
           insist { input.dependencies }.include?(dep)
         end
-      end
-
-      it "should ignore versions and conditions in 'provides' (#280)" do
-        # Provides is an array because rpm supports multiple 'provides'
-        insist { input.provides }.include?(original.name)
       end
 
       it "should fix capitalization and underscores-to-dashes (#591)" do

spec/conversion/gem_to_deb.rb
@@ -1,0 +1,36 @@
+require "spec_setup"
+require "fpm/command" # local
+require "fpm/package/deb" # local
+require "fpm/package/gem" # local
+require "stud/temporary"
+
+describe "-s gem -t deb" do
+  # dpkg-deb lets us query deb package files.
+  # Comes with debian and ubuntu systems.
+  have_dpkg_deb = program_exists?("dpkg-deb")
+  if !have_dpkg_deb
+    Cabin::Channel.get("rspec") \
+      .warn("Skipping some deb tests because 'dpkg-deb' isn't in your PATH")
+  end
+
+  let(:fpm) { FPM::Command.new("fpm") }
+
+  let(:target) { Stud::Temporary.pathname + ".deb" }
+
+  after do
+    File.unlink(target) if File.exist?(target)
+  end
+
+  before do
+    insist { fpm.run(["-s", "gem", "-t", "deb", "-p", target, "rails"]) } == 0
+  end
+
+  it "should have a correctly formatted Provides field" do
+    deb = FPM::Package::Deb.new
+    deb.input(target)
+
+    # Converting gem->deb should format the deb Provides field as "rubygem-rails (= version)"
+    insist { deb.provides.first } =~ /^rubygem-rails \(= \d+\.\d+\.\d+\)$/
+  end
+
+end # describe "-s gem -t deb"

templates/deb.erb
@@ -25,7 +25,7 @@ Build-Depends: <%= attributes[:deb_build_depends].collect { |d| fix_dependency(d
 <% if !provides.empty? -%>
 <%# Turn each provides from 'foo = 123' to simply 'foo' because Debian :\ -%>
 <%#  http://www.debian.org/doc/debian-policy/ch-relationships.html -%>
-Provides: <%= provides.map {|p| p.split(" ").first}.join ", " %>
+Provides: <%= provides.join ", " %>
 <% end -%>
 <% if !replaces.empty? -%>
 Replaces: <%= replaces.collect { |d| fix_dependency(d) }.flatten.join(", ") %>

commit 7ecdbef1160e6f2705336c93ca921ceb5edc25b1

Reject invalid Provides fields for Debian packages
This resolves an issue caused by #1803 where a user was, historically, passing `--provides 'foo (<< 1.2.3)'` which was working correctly in prior versions of fpm but creating invalid Debian packages in the newer release.

This is a funny issue because previously fpm was removing the relationship text '(<< 1.2.3)' so it never made it into the resulting Debian package. Due to #1803, this text is now passed into the resulting package, and Debian package tooling rejects it.

Added tests to cover a few valid and invalid cases.

This change also adds code to validate other relationship fields (Depends, Suggests, etc) but does not actually do any validation.

lib/fpm/package/deb.rb
@@ -25,6 +25,17 @@ class FPM::Package::Deb < FPM::Package
   # The list of supported compression types. Default is gz (gzip)
   COMPRESSION_TYPES = [ "gz", "bzip2", "xz", "none" ]
 
+  # https://www.debian.org/doc/debian-policy/ch-relationships.html#syntax-of-relationship-fields
+  # Example value with version relationship: libc6 (>= 2.2.1)
+  # Example value: libc6
+
+  # Version string docs here: https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-version
+  # The format is: [epoch:]upstream_version[-debian_revision].
+  # epoch - This is a single (generally small) unsigned integer
+  # upstream_version - must contain only alphanumerics 6 and the characters . + - ~
+  # debian_revision - only alphanumerics and the characters + . ~
+  RELATIONSHIP_FIELD_PATTERN = /^(?<name>[A-z0-9_-]+)(?: *\((?<relation>[<>=]+) *(?<version>(?:[0-9]+:)?[0-9A-Za-z+~.-]+(?:-[0-9A-Za-z+~.]+)?)\))?$/
+
   option "--ignore-iteration-in-dependencies", :flag,
             "For '=' (equal) dependencies, allow iterations on the specified " \
             "version. Default is to be specific. This option allows the same " \
@@ -427,6 +438,12 @@ class FPM::Package::Deb < FPM::Package
 
   def output(output_path)
     self.provides = self.provides.collect { |p| fix_provides(p) }
+
+    self.provides.each do |provide|
+      if !valid_provides_field?(provide)
+        raise FPM::InvalidPackageConfiguration, "Found invalid Provides field values (#{provide.inspect}). This is not valid in a Debian package."
+      end
+    end
     output_check(output_path)
     # Abort if the target path already exists.
 
@@ -773,6 +790,32 @@ class FPM::Package::Deb < FPM::Package
       return dep.rstrip
     end
   end # def fix_dependency
+
+  def valid_provides_field?(text)
+    m = RELATIONSHIP_FIELD_PATTERN.match(text)
+    if m.nil?
+      logger.error("Invalid relationship field for debian package: #{text}")
+      return false
+    end
+
+    # Per Debian Policy manual, https://www.debian.org/doc/debian-policy/ch-relationships.html#syntax-of-relationship-fields
+    # >> The relations allowed are <<, <=, =, >= and >> for strictly earlier, earlier or equal,
+    # >> exactly equal, later or equal and strictly later, respectively. The exception is the
+    # >> Provides field, for which only = is allowed
+    if m["relation"] == "=" || m["relation"] == nil
+      return true
+    end
+    return false
+  end
+
+  def valid_relationship_field?(text)
+    m = RELATIONSHIP_FIELD_PATTERN.match(text)
+    if m.nil?
+      logger.error("Invalid relationship field for debian package: #{text}")
+      return false
+    end
+    return true
+  end
 
   def fix_provides(provides)
     name_re = /^[^ \(]+/

spec/fpm/package/deb_spec.rb
@@ -397,6 +397,56 @@ describe FPM::Package::Deb do
     end
   end
 
+  describe "#output with Provides values" do
+    let(:original) { FPM::Package::Deb.new }
+
+    before do
+      # output a package, use it as the input, set the subject to that input
+      # package. This helps ensure that we can write and read packages
+      # properly.
+
+      original.name = "name"
+      original.version = "123"
+    end
+
+    after do
+      original.cleanup
+    end # after
+
+    invalid = [
+      "this is not valid",
+      "hello = world",
+      "hello ()",
+      "hello (>)",
+      "hello (1234)",
+      "foo (<< 1.0.0-54)"
+    ]
+
+    valid = [
+      "libc",
+      "libc (= 6)",
+      "bar (= 1:1.0)",
+      "bar (= 1:1.0-1)",
+      "foo (= 1.0.0-54)"
+    ]
+
+    invalid.each do |i|
+      it "should reject '#{i}'" do
+        original.provides << i
+
+        insist { original.output(target) }.raises FPM::InvalidPackageConfiguration
+      end
+    end
+
+    valid.each do |i|
+      it "should accept '#{i}'" do
+        original.provides << i
+
+        original.output(target)
+      end
+    end
+  end
+
   describe "#reproducible" do
 
     let(:package) {