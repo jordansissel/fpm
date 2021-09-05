16f1b4357f4e022124414e45d280cb49f65d478b
Jordan Sissel <jls@semicomplete.com>
2021-09-04 22:48:29 -0700

    Reject invalid Provides fields for Debian packages
    This resolves an issue caused by #1803 where a user was, historically, passing `--provides 'foo (<< 1.2.3)'` which was working correctly in prior versions of fpm but creating invalid Debian packages in the newer release.
    
    This is a funny issue because previously fpm was removing the relationship text '(<< 1.2.3)' so it never made it into the resulting Debian package. Due to #1803, this text is now passed into the resulting package, and Debian package tooling rejects it.
    
    Added tests to cover a few valid and invalid cases.
    
    This change also adds code to validate other relationship fields (Depends, Suggests, etc) but does not actually do any validation.

--- lib/fpm/package/deb.rb
+++ lib/fpm/package/deb.rb
@@ -25,6 +25,17 @@ class FPM::Package::Deb < FPM::Package
   # The list of supported compression types. Default is gz (gzip)
   COMPRESSION_TYPES = [ "gz", "bzip2", "xz", "none" ]
 
+  # https://www.debian.org/doc/debian-policy/ch-relationships.html#syntax-of-relationship-fields
+  # Example value with version relationship: libc6 (>= 2.2.1)
+  # Example value: libc6
+
+  # Version string docs here: https://www.debian.org/doc/debian-policy/ch-controlfields.html#s-f-version
+  # The format is: [epoch:]upstream_version[-debian_revision].
+  # epoch - This is a single (generally small) unsigned integer
+  # upstream_version - must contain only alphanumerics 6 and the characters . + - ~
+  # debian_revision - only alphanumerics and the characters + . ~
+  RELATIONSHIP_FIELD_PATTERN = /^(?<name>[A-z0-9_-]+)(?: *\((?<relation>[<>=]+) *(?<version>(?:[0-9]+:)?[0-9A-Za-z+~.-]+(?:-[0-9A-Za-z+~.]+)?)\))?$/
+
   option "--ignore-iteration-in-dependencies", :flag,
             "For '=' (equal) dependencies, allow iterations on the specified " \
             "version. Default is to be specific. This option allows the same " \
@@ -427,6 +438,12 @@ class FPM::Package::Deb < FPM::Package
 
   def output(output_path)
     self.provides = self.provides.collect { |p| fix_provides(p) }
+
+    self.provides.each do |provide|
+      if !valid_provides_field?(provide)
+        raise FPM::InvalidPackageConfiguration, "Found invalid Provides field values (#{provide.inspect}). This is not valid in a Debian package."
+      end
+    end
     output_check(output_path)
     # Abort if the target path already exists.
 
@@ -773,6 +790,32 @@ class FPM::Package::Deb < FPM::Package
       return dep.rstrip
     end
   end # def fix_dependency
+
+  def valid_provides_field?(text)
+    m = RELATIONSHIP_FIELD_PATTERN.match(text)
+    if m.nil?
+      logger.error("Invalid relationship field for debian package: #{text}")
+      return false
+    end
+
+    # Per Debian Policy manual, https://www.debian.org/doc/debian-policy/ch-relationships.html#syntax-of-relationship-fields
+    # >> The relations allowed are <<, <=, =, >= and >> for strictly earlier, earlier or equal,
+    # >> exactly equal, later or equal and strictly later, respectively. The exception is the
+    # >> Provides field, for which only = is allowed
+    if m["relation"] == "=" || m["relation"] == nil
+      return true
+    end
+    return false
+  end
+
+  def valid_relationship_field?(text)
+    m = RELATIONSHIP_FIELD_PATTERN.match(text)
+    if m.nil?
+      logger.error("Invalid relationship field for debian package: #{text}")
+      return false
+    end
+    return true
+  end
 
   def fix_provides(provides)
     name_re = /^[^ \(]+/
