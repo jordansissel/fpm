d30c9592c99df4e2421daacab36dd325dee57184
Jordan Sissel <jls@semicomplete.com>
2021-11-09 21:00:22 -0800

    'empty' packages now default to "all" architecture.
    This feels like the right default because empty packages have no files
    (especially no binary, architecture-specific files) and therefore should
    be installable on any architecture.
    
    Fixes #1846

--- lib/fpm/package/empty.rb
+++ lib/fpm/package/empty.rb
@@ -4,6 +4,18 @@ require "backports/latest"
 # Empty Package type. For strict/meta/virtual package creation
 
 class FPM::Package::Empty < FPM::Package
+  def initialize(*args)
+    super(*args)
+
+    # Override FPM::Package's default "native" architecture value
+    # This feels like the right default because an empty package has no
+    # architecture-specific files, and in most cases an empty package should be
+    # installable anywhere.
+    #
+    # https://github.com/jordansissel/fpm/issues/1846
+    @architecture = "all"
+  end
+
   def output(output_path)
     logger.warn("Your package has gone into the void.")
   end
