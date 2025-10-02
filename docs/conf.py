a066c4447d53d6c9f7b3d0332c4e6f2767857fe5
Jordan Sissel <jls@semicomplete.com>
2025-10-02 12:20:32 -0700

    Try to fix another error
    >     sphinx.errors.ThemeError: no theme named 'sphinx_rtd_theme' found (missing theme.toml?)

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,9 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'sphinx_rtd_theme'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
